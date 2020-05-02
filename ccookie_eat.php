bc0447b923e833345b3f8e70d79951746668aebe
qhanlon <quinn.hanlon@gmail.com>
2020-05-01 17:53:31 -0700

    Cookie Eat Supression

--- ccookie_eat.php
+++ ccookie_eat.php
@@ -1,8 +1,8 @@
 
 <?php 
-    echo "<p>" . $_COOKIE['username'] . "</p>";
-    echo "<p>" . $_COOKIE['firstname'] . "</p>";
-    echo "<p>" . $_COOKIE['lastname'] . "</p>";
+    echo isset($_COOKIE['username']) ? "<p>" . $_COOKIE['username'] . "</p>" : "<p>No username is set.</p>";
+    echo isset($_COOKIE['firstname']) ? "<p>" . $_COOKIE['firstname'] . "</p>" : "<p>No first name is set.</p>";
+    echo isset($_COOKIE['lastname']) ? "<p>" . $_COOKIE['lastname'] . "</p>" : "<p>No last name is set.</p>";
     if (isset($_COOKIE['cookie'])) {
         foreach ($_COOKIE['cookie'] as $key=>$val) {
             echo $key . ' is ' . $val."<br>\n";
